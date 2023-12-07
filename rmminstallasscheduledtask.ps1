2a5ac9be5a1d6f66bb59afea2681f0e189a074df
DavidAamcomp <87546072+DavidAamcomp@users.noreply.github.com>
2023-12-07 13:33:24 -0600

    Create rmminstallasscheduledtask.ps1

---
+++ rmminstallasscheduledtask.ps1
@@ -0,0 +1,8 @@
+# Create an action to run the PowerShell script
+$action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument '-NoProfile -ExecutionPolicy Bypass -File "del c:\windows\temp\rmm.ps1; Invoke-WebRequest -URI https://raw.githubusercontent.com/DavidAamcomp/Scripts/main/rmm.ps1 -OutFile c:\Windows\temp\rmm.ps1; Set-ExecutionPolicy Bypass -Scope Process -Force; c:\Windows\temp\rmm.ps1"'
+
+# Create a trigger for midnight
+$trigger = New-ScheduledTaskTrigger -Daily -At "00:00"
+
+# Register the scheduled task without specifying a password and with elevated privileges
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "RunScriptAtMidnight" -User "NT AUTHORITY\SYSTEM" -Force
