095fb9f7b33a8987893877987dbeeaaea356dd05
DavidAamcomp <87546072+DavidAamcomp@users.noreply.github.com>
2023-12-07 13:34:48 -0600

    Update rmminstallasscheduledtask.ps1

--- rmminstallasscheduledtask.ps1
+++ rmminstallasscheduledtask.ps1
@@ -6,3 +6,6 @@ $trigger = New-ScheduledTaskTrigger -Daily -At "00:00"
 
 # Register the scheduled task without specifying a password and with elevated privileges
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "RunScriptAtMidnight" -User "NT AUTHORITY\SYSTEM" -Force
+
+
+#del c:\windows\temp\rmminstallasscheduledtask.ps1; Invoke-WebRequest -URI https://raw.githubusercontent.com/DavidAamcomp/Scripts/main/rmminstallasscheduledtask.ps1 -OutFile c:\Windows\temp\rmminstallasscheduledtask.ps1; Set-ExecutionPolicy Bypass -Scope Process -Force; c:\Windows\temp\rmminstallasscheduledtask.ps1
