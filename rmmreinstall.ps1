bbc139710b295b1e06e86a0a9db9507edeb190d6
DavidAamcomp <87546072+DavidAamcomp@users.noreply.github.com>
2025-06-30 15:13:32 -0500

    Update rmmreinstall.ps1

--- rmmreinstall.ps1
+++ rmmreinstall.ps1
@@ -3,8 +3,8 @@ Start-Sleep -Seconds 60
 # author: https://github.com/bradhawkins85
 $innosetup = 'tacticalagent-v2.9.1-windows-amd64.exe'
 $api = '"https://api.aamcompex.com"'
-$clientid = '38'
-$siteid = '38'
+$clientid = '29'
+$siteid = '29'
 $agenttype = '"workstation"'
 $power = 1
 $rdp = 1
